f3cbf3159a79058b5ec1595a87b9efb46308c973
Carlos Peña <cgabriel2303@gmail.com>
2025-12-14 22:46:31 -0300

    add image on product list

--- frontend/src/app/pages/items/items.component.ts
+++ frontend/src/app/pages/items/items.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApiService } from '../../services/api.service';
+import { environment } from '../../../environments/environment';
+
 
 @Component({
   selector: 'app-items',
@@ -11,14 +13,20 @@ import { ApiService } from '../../services/api.service';
 export class ItemsComponent implements OnInit {
 
   items: any[] = [];
+  apiBaseUrl = environment.apiUrl.replace('/api', '');
 
-  constructor(private api: ApiService) {}
+
+  constructor(private api: ApiService) { }
 
   loadItems() {
-  this.api.getItems().subscribe(data => this.items = data);
-}
+    this.api.getItems().subscribe(data => this.items = data);
+  }
 
-ngOnInit() {
-  this.loadItems();
-}
+  ngOnInit() {
+    this.loadItems();
+  }
+   getImageUrl(imagePath: string | null): string {
+    if (!imagePath) return 'assets/placeholder-no-image.png';    
+    return `${this.apiBaseUrl}${imagePath}`;
+  }
 }
